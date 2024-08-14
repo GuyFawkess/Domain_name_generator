0c31107346fc0d0d8c9144eaf49cffde3817d347
Jason the noob <jason-ollivierre@hotmail.com>
2024-08-14 18:42:59 +0100

    añadi tambien los dominios por hacer tiempo

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let dom = ['.com', '.net', '.us', '.io'];
 
 let domains = pronoun.flatMap(p => 
     adj.flatMap(a => 
-        noun.flatMap(n => `${p} ${a} ${n}.com`)
+        noun.flatMap(n => 
+            dom.flatMap(d => `${p} ${a} ${n} ${d}`)
+        )
     )
 )
 
